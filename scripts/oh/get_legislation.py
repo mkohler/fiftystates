ac7262321b609571be717ddcd854dda7683fe68b
Mark Kohler <kohler@netrino.com>
2009-03-31 12:15:12 -0500

    Fixed many problems with main URL retrieval loop.

--- scripts/oh/get_legislation.py
+++ scripts/oh/get_legislation.py
@@ -11,7 +11,7 @@ import sys
 sys.path.append('./scripts/pyutils')
 import legislation
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)s-%(message)s')
 
 # ToDo
 #
@@ -25,10 +25,21 @@ class OhioBill(object):
         self.number = number
         self.filename = self.make_filename()
         self.id = self.make_id()
-        self.url = self.make_url_style1()
+        self.url = self.make_url_clean_html()
         self.name = None
         self.version_name = None
         self.text = None
+
+    def has_bill_text(self):
+        if not self.text:
+            return False
+        if 'could not be found' in self.text:
+            return False
+        if 'You have requested a page that does not exist' in self.text:
+            return False
+        if 'Bad Request' in self.text:
+            return False
+        return True
 
     def make_id(self):
         if self.chamber == 'lower':
@@ -41,34 +52,63 @@ class OhioBill(object):
         return 'data/oh/%s_%s_%s.html' % (self.session, self.chamber,
                                           self.number)
 
-    def make_url_style1(self):
+    def make_url_clean_html(self):
         if self.chamber == 'lower':
             id = 'HB_%s' % self.number
         else:
             id = 'SB_%s' % self.number
 
         return ('http://www.legislature.state.oh.us/' +
-                'BillText%s/%s_%s_I_N.html' % 
+                'BillText%s/%s_%s_1_N.html' %
                 (self.session, self.session, self.id))
 
-    def make_url_style2(self):
-        if self.chamber == 'upper':
+    def make_url_with_framing(self):
+        if self.chamber == 'lower':
             return ( 'http://www.legislature.state.oh.us/' +
                      'bills.cfm?ID=%s_HB_%s' % (self.session, self.number))
         else:
             return ('http://www.legislature.state.oh.us/' +
                      'bills.cfm?ID=%s_SB_%s' % (self.session, self.number))
 
+    def parse_bill(self):
+        soup = BeautifulSoup(self.text)
+
+        # XXX 
+        #
+        # self.add_bill(chamber, session, bill_id, bill_title)
+
+        # XXX
+        # self.add_bill_version(chamber, session, bill_id, version_name,
+        # version_url)
+
+        # XXX
+        # self.add_sponsorship(chamber, session, bill_id, 'primary', leg)
+
+        # XXX 
+        # self.add_sponsorship(chamber, session, bill_id, 'cosponsor', leg)
+
+        # XXX
+        # self.add_action(chamber, session, bill_id, action_chamber, action,
+        # date)
+
+
+    #
+    # For some bills, we can get the full text in a clean HTML format. We
+    # try that approach first, but if it fails, we fall back to getting the
+    # text along with the headers and menus.
+    #
     def retrieve_bill_text(self):
-        logging.info('Retrieving %s' % self.url)
         self.text = urllib.urlopen(self.url).read()
 
         if self.has_bill_text():
+            logging.info('Retrieved %s' % self.url)
             return
+        else:
+            # For many bills, this is an expected failure.
+            logging.debug('%s failed' % self.url)
 
-        self.url = self.make_url_style2()
-        logging.info('First URL failed, trying another URL style...')
-        logging.info('Retrieving %s' % self.url)
+        self.url = self.make_url_with_framing()
+        logging.info('Retrieved %s' % self.url)
         self.text = urllib.urlopen(self.url).read()
         if self.has_bill_text():
             return
@@ -76,17 +116,6 @@ class OhioBill(object):
         logging.warn('Could not find bill: chamber %s, year %s, number %s'
                      % (self.chamber, self.year, self.number))
         raise legislation.NoDataForYear(self.year)
-
-    def has_bill_text(self):
-        if not self.text:
-            return False
-        if 'could not be found' in self.text:
-            return False
-        if 'You have requested a page that does not exist' in self.text:
-            return False
-        if 'Bad Request' in self.text:
-            return False
-        return True
 
     def save_bill_text_as_file(self):
         f = open(self.filename, 'w')
@@ -111,7 +140,10 @@ class OHLegislationScraper(legislation.LegislationScraper):
 
             # If we don't have the bill, go get it and save it.
             if not os.path.isfile(bill.filename):
-                bill.retrieve_bill_text()
+                try:
+                    bill.retrieve_bill_text()
+                except legislation.NoDataForYear:
+                    break
                 bill.save_bill_text_as_file()
                 time.sleep(1)            # Give the poor web server a break.
             else:
@@ -121,26 +153,6 @@ class OHLegislationScraper(legislation.LegislationScraper):
             bill_number += 1
 
 
-def parse_bill(session, chamber, bill_number, bill_html):
-    bill_soup = BeautifulSoup(bill_html)
-
-    # XXX 
-    #
-    # self.add_bill(chamber, session, bill_id, bill_title)
-
-    # XXX
-    # self.add_bill_version(chamber, session, bill_id, version_name,
-    # version_url)
-
-    # XXX
-    # self.add_sponsorship(chamber, session, bill_id, 'primary', leg)
-
-    # XXX 
-    # self.add_sponsorship(chamber, session, bill_id, 'cosponsor', leg)
-
-    # XXX
-    # self.add_action(chamber, session, bill_id, action_chamber, action,
-    # date)
 
 
 def year_to_session(year):
